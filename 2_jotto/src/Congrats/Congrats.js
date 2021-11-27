0fb61bdce0baae75713168312a1b729e98f2e2c3
Vishal Sanserwal <vsanse@outlook.com>
2021-11-27 21:42:11 +0530

    congrats tests completed

---
+++ 2_jotto/src/Congrats/Congrats.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+/**
+ * Functional react component for congratulatory message.
+ * @function
+ * @param {object} props - React props.
+ * @returns {JSX.Element} - Rendered component (or null if `success` prop is false).
+ */
+export default function Congrats({ success }) {
+  return (
+    <div data-test="component-congrats">
+      {success && (
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word!
+        </span>
+      )}
+    </div>
+  );
+}
